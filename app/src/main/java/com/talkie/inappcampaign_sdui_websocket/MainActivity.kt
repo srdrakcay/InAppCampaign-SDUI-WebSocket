730dc018b747f7e27a23148f266419d773baf0f6
srdrakcay <srdrakcay25@gmail.com>
2024-07-29 23:46:47 +0300

    sample json

--- app/src/main/java/com/talkie/inappcampaign_sdui_websocket/MainActivity.kt
+++ app/src/main/java/com/talkie/inappcampaign_sdui_websocket/MainActivity.kt
@@ -20,3 +20,54 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+/*
+{
+  "socketData": [
+    {
+      "type": "Text",
+      "paddingCard": 16,
+      "height": 150,
+      "roundedCornerShape": 16,
+      "cardElevation": 8,
+      "verticalGradient1": "Red",
+      "verticalGradient2": "Blue",
+      "painterResource": "R.drawable.ic_diamond1",
+      "alignment": "TopCenter",
+      "alignmentText": "BottomCenter",
+      "messageText": "Testt",
+      "paddingText": 40,
+      "imageSize": 120,
+      "texSize": 20,
+      "alignmentButton": "BottomEnd",
+      "cardTimeCornerRadius": 12,
+      "sizeW": 120,
+      "sizeH": 60,
+      "cardPadding": 12,
+      "cardTimeElevation": 40
+    },
+    {
+      "type": "Image",
+      "paddingCard": 16,
+      "height": 150,
+      "roundedCornerShape": 16,
+      "cardElevation": 8,
+      "verticalGradient1": "Green",
+      "verticalGradient2": "Yellow",
+      "painterResource": "R.drawable.ic_diamond1",
+      "alignment": "TopEnd",
+      "alignmentText": "TopEnd",
+      "messageText": "Sana Özel",
+      "paddingText": 40,
+      "imageSize": 140,
+      "texSize": 20,
+      "alignmentButton": "BottomEnd",
+      "paddingButton": 40,
+      "cardTimeCornerRadius": 12,
+      "sizeW": 40,
+      "sizeH": 40,
+      "cardPadding": 40,
+      "cardTimeElevation": 40
+    }
+  ]
+}
+*/
